ff13ea40e89caa0c04039bed90db2ac63e23ee9d
ramyanh2 <ramyanh2@gmail.com>
2021-09-27 16:57:32 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from "./friends/store";
 import FriendListContainer from "./friends/components/friend_list_container";
 import "./stylesheet/friend_list.css";
 
+alert("his")
 const container = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
